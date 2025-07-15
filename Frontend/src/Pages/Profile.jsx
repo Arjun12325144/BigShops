b7d889439bd35453b86aa75f8b50e4883eb592e3
Arjun12325144 <rajputarjun2901@gmail.com>
2025-07-15 18:04:21 +0530

    commit changes

--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import MyOrdersPage from './myOrdersPage'
+import MyOrdersPage from './MyOrdersPage'
 import { useDispatch , useSelector} from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import { logout } from "../redux/slices/authSlice"
